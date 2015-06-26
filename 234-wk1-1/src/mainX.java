1d73ba99edf4094bc827ef7b692532ac55e62be3
stefan bund <stefan.bund@icloud.com>
2015-06-25 19:03:53 -0700

    updated wk1-1
    new randoms and generateLetter, for you to complete for next week

--- 234-wk1-1/src/mainX.java
+++ 234-wk1-1/src/mainX.java
@@ -1,22 +1,59 @@
+import java.util.Random;
 
 public class mainX {
 
 	public static void main(String[] args)  //main is a method or function
 	{
-		int numberOfReps = 0; //declare this variable then initialize it, with a 0 number 
-		
-		System.out.println("here's the part before the for loop...");
-		
-		for(int x = 0; x <= 1500; x++) //increment, states by how much my number increases
-		{
-			System.out.println("currently I am in the " + x + "th repetition.");
-			numberOfReps = x; 
-			x = x * 2; //would me doing this cause the loop to stop
-			
-		}
-		
-		System.out.println("the number of times this algorithm repeated was " + numberOfReps);
+		Random r1 = new Random();
+		Random r2 = new Random();//create a random # generator
+		Random r3 = new Random();
+		Random r4 = new Random();
 
+		for(int x = 0; x <= 6000; x++) //repeats
+		{
+			//for each stock, determine its ticker symbol, NASDAC style, four letters	
+			double ra1 = r1.nextDouble(); //generate a number between 0 and 1 
+			double ra2 = r2.nextDouble();
+			double ra3 = r3.nextDouble();
+			double ra4 = r4.nextDouble();
+//			System.out.println("tell me the value of the random 1... "+ ra1);
+//			System.out.println("tell me the value of the random 2... "+ ra2);
+//			System.out.println("tell me the value of the random 3... "+ ra3);
+//			System.out.println("tell me the value of the random 4... "+ ra4);
+
+			//for each variable, get a random number, then based on that number, generate a new letter
+			String symbolLetter1 = generateLetter(ra1);
+			String symbolLetter2 = generateLetter(ra2);
+			String symbolLetter3 = generateLetter(ra3);
+			String symbolLetter4 = generateLetter(ra4);
+
+			
+			String symbol = symbolLetter1 + symbolLetter2 + symbolLetter3 + symbolLetter4 ;
+			System.out.println("symbol is live: " + symbol);
+		}//for loop bracket
+	}//main bracket
+
+/**
+ * @param n, a randomly generated number
+ * @return l, a string representing a letter
+ */
+	public static String generateLetter(double n)
+	{
+		String l = "";//empty, or null, to begin
+		if(n > 0 && n <= .1)
+		{
+			l = "A";
+		}
+		else if(n >= .1 && n <=.2)
+		{
+			l = "B";
+		}
+		else if(n >= .2 && n <=.3)
+		{
+			l = "C";
+		}
+
+		return l;
 	}
 
 }

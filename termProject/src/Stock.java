eed0b221fc02a3d094094331bfc91505eca4014a
stefan bund <stefan.bund@icloud.com>
2016-01-07 21:16:21 -0800

    u

--- termProject/src/Stock.java
+++ termProject/src/Stock.java
@@ -3,27 +3,21 @@
  * @author stefanbund
  *
  */
-public class Stock {
-	
-	public String symbol; //a public variable is visible outside the class, by other classes
-	public double price;  //55.89
-	public int ipoQty; //initial number of stock on sale
+public class Stock 
+{	
+	public String symbol; 		//a public variable is visible outside the class, by other classes
+	public double price;  		//55.89
+	public int ipoQty;		 //initial number of stock on sale
 	public String industry; 
 	
-	public Stock() //constructor, first thing that happens, when an instance of this object
-					//is created 
+							//constructor, first thing that happens, when an instance of this object
+	public Stock()       	// POPULATE THIS STOCK WITH SOME INFORMAITON: 
 	{
-		// POPULATE THIS STOCK WITH SOME INFORMAITON: 
 		System.out.println("I just called the constructor....");
-		
 		industry = "defense contracting"; //string, or word, which is a string of characters
-		
-		price = 55.0; //price is a double, is a number, with decimals
-		
-		ipoQty = 50000000; //an integer, no decimals!
-		
+		price = 55.67; //price is a double, is a number, with decimals
+		ipoQty = 50000001; //an integer, no decimals!
 		symbol = "GEL"; //for our sim, we'll assign randomly determined symbol names
-		
 	}
 
 }
--- termProject/src/brainCenter.java
+++ termProject/src/brainCenter.java
@@ -1,32 +1,40 @@
 
-public class brainCenter {
+public class brainCenter 
+{
 
 	public static void main(String[] args) 
-	{ 										//main compilation unit, runs first
-		
+	{ 											//main compilation unit, runs first
 		System.out.println("MAIN BEGINNING NOW......");
 		
-		Stock st = new Stock(); //your innovation
+		Stock st = new Stock(); 				
+		String ind = "telecom"; 				//test this below: 
 		
-		
-		String ind = "telecom"; //test this below: 
-		
-		if(st.industry.equalsIgnoreCase(ind))
-		{
-			System.out.println("the industry of this stock is telecom");
-
-		}
-		else
-		{
-			System.out.println("the industry of this stock is not telecom,");
-			System.out.println("the industry of this stock is: " + st.industry);
-
-
-		}
-		
-
+			if(st.industry.equalsIgnoreCase(ind))
+			{
+				System.out.println("the industry of this stock is telecom");
+			}
+			else
+			{
+				System.out.println("the industry of this stock is not telecom,");
+				System.out.println("the industry of this stock is: " + st.industry);
+			}
+			
+			int limit = 1000; 				//INTEGER TESTING
+			if(st.ipoQty < limit)
+			{
+				System.out.println("IPO QTY EXCEEDS LIMIT OF " + limit);
+			}
+			else if(st.ipoQty <= limit)
+			{
+				System.out.println("IPO QTY FALLS WITHIN THE LIMIT " + limit);
+			}
+			else if(st.ipoQty <= 50000000)
+			{
+				System.out.println("YOUR IPO QTY IS == 50000000");
+			}
+			else
+			{
+				System.out.println("SORRY, NOTHING MATCHED......");
+			}
 	}
-	
-	
-
 }
